fbdef29399657309b04b24749d033b8d8dd82c00
xiaofang.wu <983053245@qq.com>
2024-06-15 14:59:52 +0800

    mq中间件测试

--- develop-sample-server/src/main/java/com/sxnd/develop/sample/mq/RabbitMqConfig.java
+++ develop-sample-server/src/main/java/com/sxnd/develop/sample/mq/RabbitMqConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,84 +19,84 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Slf4j
 public class RabbitMqConfig {
-//    /**
-//     *  调度任务状态的队列
-//     * @return  任务状态的队列-IOT订阅
-//     */
-//    @Bean("dispatchTaskStatusQueueToDevice")
-//    public Queue DispatchTaskStatusQueueToDevice() {
-//        return new Queue("dispatchTaskStatusQueue.ToDevice");
-//    }
-//
-//    /**
-//     *  调度任务下发队列
-//     * @return 路权订阅
-//     */
-//    @Bean("dispatchTaskContentQueueToRouteRight")
-//    public Queue dispatchTaskContentQueueToRouteRight() {
-//        return new Queue("dispatchTaskContentQueue.ToRouteRight");
-//    }
-//
-//    /**
-//     * 调度任务下发队列
-//     * @return IOT订阅
-//     */
-//    @Bean("dispatchTaskContentQueueToDevice")
-//    public Queue DispatchTaskContentQueueToDevice() {
-//        return new Queue("dispatchTaskContentQueue.ToDevice");
-//    }
-//
-//    /**
-//     * 调度任务状态主题交换机
-//     * @return
-//     */
-//    @Bean("dispatchTaskStatusTopic")
-//    public TopicExchange dispatchTaskStatusTopicExchange() {
-//        return new TopicExchange("dispatchTaskStatus.Topic");
-//    }
-//
-//    /**
-//     * 调度任务下发主题交换机
-//     * @return
-//     */
-//    @Bean("dispatchTaskContentTopic")
-//    public TopicExchange taskContentTopicExchange() {
-//        return new TopicExchange("dispatchTaskContent.Topic");
-//    }
-//
-//    /**
-//     * 调度任务状态队列与主题交换机的绑定
-//     * @param topic
-//     * @param dispatchTaskStatusQueueToDevice
-//     * @return
-//     */
-//    @Bean
-//    public Binding bindingDispatchTaskStatusQueueToDevice(@Qualifier("dispatchTaskStatusTopic") TopicExchange topic, @Qualifier("dispatchTaskStatusQueueToDevice") Queue dispatchTaskStatusQueueToDevice) {
-//        return BindingBuilder.bind(dispatchTaskStatusQueueToDevice)
-//                .to(topic)
-//                .with("dispatch.taskStatus");
-//    }
-//
-//
-//    /**
-//     * 调度任务下发队列与主题交换机的绑定
-//     * @param topic
-//     * @param dispatchTaskContentQueueToDevice
-//     * @return
-//     */
-//    @Bean
-//    public Binding bindingTopicToDeviceTaskContentQueue(@Qualifier("dispatchTaskContentTopic") TopicExchange topic, @Qualifier("dispatchTaskContentQueueToDevice") Queue dispatchTaskContentQueueToDevice) {
-//        return BindingBuilder.bind(dispatchTaskContentQueueToDevice)
-//                .to(topic)
-//                .with("dispatch.taskContent");
-//    }
-//
-//    @Bean
-//    public Binding  bindingTopicToRouteRightTaskContentQueue(@Qualifier("dispatchTaskContentTopic") TopicExchange topic, @Qualifier("dispatchTaskContentQueueToRouteRight") Queue dispatchTaskContentQueueToRouteRight) {
-//        return BindingBuilder.bind(dispatchTaskContentQueueToRouteRight)
-//                .to(topic)
-//                .with("dispatch.taskContent.routeRight");
-//    }
+    /**
+     *  调度任务状态的队列
+     * @return  任务状态的队列-IOT订阅
+     */
+    @Bean("dispatchTaskStatusQueueToDevice")
+    public Queue DispatchTaskStatusQueueToDevice() {
+        return new Queue("dispatchTaskStatusQueue.ToDevice");
+    }
+
+    /**
+     *  调度任务下发队列
+     * @return 路权订阅
+     */
+    @Bean("dispatchTaskContentQueueToRouteRight")
+    public Queue dispatchTaskContentQueueToRouteRight() {
+        return new Queue("dispatchTaskContentQueue.ToRouteRight");
+    }
+
+    /**
+     * 调度任务下发队列
+     * @return IOT订阅
+     */
+    @Bean("dispatchTaskContentQueueToDevice")
+    public Queue DispatchTaskContentQueueToDevice() {
+        return new Queue("dispatchTaskContentQueue.ToDevice");
+    }
+
+    /**
+     * 调度任务状态主题交换机
+     * @return
+     */
+    @Bean("dispatchTaskStatusTopic")
+    public TopicExchange dispatchTaskStatusTopicExchange() {
+        return new TopicExchange("dispatchTaskStatus.Topic");
+    }
+
+    /**
+     * 调度任务下发主题交换机
+     * @return
+     */
+    @Bean("dispatchTaskContentTopic")
+    public TopicExchange taskContentTopicExchange() {
+        return new TopicExchange("dispatchTaskContent.Topic");
+    }
+
+    /**
+     * 调度任务状态队列与主题交换机的绑定
+     * @param topic
+     * @param dispatchTaskStatusQueueToDevice
+     * @return
+     */
+    @Bean
+    public Binding bindingDispatchTaskStatusQueueToDevice(@Qualifier("dispatchTaskStatusTopic") TopicExchange topic, @Qualifier("dispatchTaskStatusQueueToDevice") Queue dispatchTaskStatusQueueToDevice) {
+        return BindingBuilder.bind(dispatchTaskStatusQueueToDevice)
+                .to(topic)
+                .with("dispatch.taskStatus");
+    }
+
+
+    /**
+     * 调度任务下发队列与主题交换机的绑定
+     * @param topic
+     * @param dispatchTaskContentQueueToDevice
+     * @return
+     */
+    @Bean
+    public Binding bindingTopicToDeviceTaskContentQueue(@Qualifier("dispatchTaskContentTopic") TopicExchange topic, @Qualifier("dispatchTaskContentQueueToDevice") Queue dispatchTaskContentQueueToDevice) {
+        return BindingBuilder.bind(dispatchTaskContentQueueToDevice)
+                .to(topic)
+                .with("dispatch.taskContent");
+    }
+
+    @Bean
+    public Binding  bindingTopicToRouteRightTaskContentQueue(@Qualifier("dispatchTaskContentTopic") TopicExchange topic, @Qualifier("dispatchTaskContentQueueToRouteRight") Queue dispatchTaskContentQueueToRouteRight) {
+        return BindingBuilder.bind(dispatchTaskContentQueueToRouteRight)
+                .to(topic)
+                .with("dispatch.taskContent.routeRight");
+    }
 
     @Bean
     public RabbitTemplate createRabbitTemplate(ConnectionFactory connectionFactory) {
